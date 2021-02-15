9a35c1363f528fcd1c7610ba2692f58c061de7eb
Jacob <jacoblindev@gmail.com>
2021-02-15 22:21:10 +0800

    Initial Commit...

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        title: 'Exclusive Chat...',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Exclusive Chat'),
+          ),
+        ),
+      ),
+    );
