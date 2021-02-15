22e6f98a86386eb2d998103e2634741e127ec174
Jacob <jacoblindev@gmail.com>
2021-02-15 23:09:41 +0800

    Finished the TextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,70 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        title: 'Exclusive Chat...',
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Exclusive Chat'),
-          ),
+      ExclusiveChatApp(),
+    );
+
+class ExclusiveChatApp extends StatelessWidget {
+  const ExclusiveChatApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Exclusive Chat...',
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Exclusive Chat'),
+      ),
+      body: _buildTextComposer(),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    InputDecoration.collapsed(hintText: 'Send a message'),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+}
